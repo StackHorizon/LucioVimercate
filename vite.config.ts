449ff188e4eadbc05209481a9b85d7b6c7dac00e
Ciro Andrea Strazzullo <ciro@MacBook-Air.station>
2025-07-15 12:07:40 +0200

    Prova pubblicazione

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/LucioVimercate/' : '/',
+  base: mode === 'production' ? '/LucioVimercate/' : '/',
   server: {
     host: "::",
     port: 8080,
